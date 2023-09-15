16c1300cb3a2d72d05c222cf6402526ae4b75686
charitympmb <charitympmb@gmail.com>
2023-09-15 13:44:07 +0200

    web-version-2 Html, Css, Javascript done

---
+++ main.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    // $(window).scroll(function(){
+        
+    // })
+
+
+
+    // For typing Animation
+    var chariType1 = new Typed(".typing", {
+        strings: [
+            "Junior Developer",
+            "Charity Mapamba",
+            "in Germany"
+        ],
+        typeSpeed: 50,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    var charity22 = new Typed(".typing-2",{
+        strings: [
+            "Mapamba"
+        ],
+        typeSpeed: 50,
+        backSpeed: 55,
+        loop: true
+    });
+
+    var charity3 = new Typed(".ptype",{
+        strings: [
+            "Work in Team, an ready to work in a Company"
+        ],
+        typeSpeed: 50,
+        backSpeed: 55,
+        loop: true
+    });
+    
+    var charity23 = new Typed(".typing3",{
+        strings: [
+            "Development Skills"
+        ],
+        typeSpeed: 50,
+        backSpeed: 55,
+        loop: true
+    });
+
+
+
+    // styling Owl Carousel
+    /*$(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    })*/
+
+})
